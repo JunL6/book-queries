e128f5ed091b70c1da7598b6d07640a1c601d19e
leonardoj6 <l.jun1717@gmail.com>
2020-11-20 11:14:54 -0600

    refined file structure

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,3 +1,4 @@
-const URL_MONGODB = 'mongodb+srv://leonardo:test123@cluster0.4dvid.mongodb.net/bookstore?retryWrites=true&w=majority'
+const URL_MONGODB =
+	"mongodb+srv://leonardo:test123@cluster0.4dvid.mongodb.net/bookstore?retryWrites=true&w=majority";
 
 module.exports = URL_MONGODB;
